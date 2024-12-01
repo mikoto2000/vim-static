b77381a7429619ae56b5d529bc9390195667b675
mikoto2000 <mikoto2000@gmail.com>
2024-12-01 21:32:23 +0900

    Updated build.sh.
    Add LICENSE in tar.gz.

--- build.sh
+++ build.sh
@@ -8,8 +8,11 @@
 TAG_NAME=$1
 ARCH=$2
 
+# ビルド
 cd ${ARCH}
 nix-build --cores $(nproc)
+
+# エントリーポイント作成
 mv result vim-${TAG_NAME}-${ARCH}
 cd vim-${TAG_NAME}-${ARCH}
 cat << "EOF" > ./AppRun
@@ -19,6 +22,11 @@ export VIM="${CURRENT_DIR}/share/vim/"
 exec "${CURRENT_DIR}/bin/vim" "$@"
 EOF
 chmod a+x ./AppRun
+
+# ライセンス添付
 cd ..
+cp ../LICENSE ./vim-${TAG_NAME}-${ARCH}
+
+# tar.gz に固める
 tar zcfvh ./vim-${TAG_NAME}-${ARCH}.tar.gz ./vim-${TAG_NAME}-${ARCH}
 
