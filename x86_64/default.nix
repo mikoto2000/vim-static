07306e3053a94de2a47c4f08849d76cf7a786a5d
mikoto2000 <mikoto2000@gmail.com>
2024-12-01 20:46:07 +0900

    Added nix scripts.

---
+++ x86_64/default.nix
@@ -0,0 +1,13 @@
+let
+  pkgs = import <nixpkgs> { };
+  pkgsStatic = pkgs.pkgsStatic;
+in
+pkgsStatic.vim.overrideAttrs {
+  version = "v9.1.0898";
+  src = pkgs.fetchFromGitHub {
+    owner = "vim";
+    repo = "vim";
+    rev = "v9.1.0898";
+    hash = "sha256-pZ1zB+c9ZQ3e1H8m5jJ4WeqWmeaHMUENpzd5DNaKtjo=";
+  };
+}
