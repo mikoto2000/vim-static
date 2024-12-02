8c9ffc382e97c94bba6e8dbe0469dd9a7cf8d7d2
mikoto2000 <mikoto2000@gmail.com>
2024-12-03 08:47:55 +0900

    ハッシュ計算処理を追加。

--- build.sh
+++ build.sh
@@ -11,8 +11,14 @@
 TAG_NAME=$1
 ARCH=$2
 
+# ハッシュの計算
+HASH=$(nix-prefetch-git --rev ${TAG_NAME} https://github.com/vim/vim | jq -r .hash)
+
 # バージョンの更新
 sed -i -e "s/v9.1.0899/${TAG_NAME}/g" ./${ARCH}/default.nix
+
+# ハッシュの更新
+sed -i -e "s/sha256-pZ1zB+c9ZQ3e1H8m5jJ4WeqWmeaHMUENpzd5DNaKtjo=/${HASH}/g" ./${ARCH}/default.nix
 
 # ビルド
 cd ${ARCH}
