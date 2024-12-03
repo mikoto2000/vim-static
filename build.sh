0aeec5b5a1ba2643f6b7bf3c28a9ce32c248cebf
mikoto2000 <mikoto2000@gmail.com>
2024-12-03 09:31:15 +0900

    ビルド成果物のディレクトリに追加するのに管理者権限が必要なので sudo を追加。

--- build.sh
+++ build.sh
@@ -27,17 +27,17 @@ nix-build --cores $(nproc)
 # エントリーポイント作成
 mv result vim-${TAG_NAME}-${ARCH}
 cd vim-${TAG_NAME}-${ARCH}
-cat << "EOF" > ./AppRun
+cat << "EOF" | sudo tee ./AppRun > /dev/null
 #!/bin/sh
 CURRENT_DIR="$(dirname "$(realpath "$0")")"
 export VIM="${CURRENT_DIR}/share/vim/"
 exec "${CURRENT_DIR}/bin/vim" "$@"
 EOF
-chmod a+x ./AppRun
+sudo chmod a+x ./AppRun
 
 # ライセンス添付
 cd ..
-cp ../LICENSE ./vim-${TAG_NAME}-${ARCH}
+sudo cp ../LICENSE ./vim-${TAG_NAME}-${ARCH}
 
 # tar.gz に固める
 tar zcfvh ./vim-${TAG_NAME}-${ARCH}.tar.gz ./vim-${TAG_NAME}-${ARCH}
